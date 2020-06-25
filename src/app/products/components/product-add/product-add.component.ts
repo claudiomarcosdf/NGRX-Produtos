1a02e5f8c42619688c4cec1c19cc7aecb779c17c
claudiomarcosdf <claudio.marcos.df@gmail.com>
2020-06-25 15:44:30 -0300

    Primeiro post - RGRX-Products

--- src/app/products/components/product-add/product-add.component.ts
+++ src/app/products/components/product-add/product-add.component.ts
@@ -1,26 +1,25 @@
-import { Component, OnInit } from "@angular/core";
-import { NgForm } from "@angular/forms";
-import { ProductService } from "../../services/product.service";
-import { Router } from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ProductService } from '../../services/product.service';
+import { Router } from '@angular/router';
+import { ProductState } from '../../store/product.reducer';
+import { Store } from '@ngrx/store';
+import { addProduct } from '../../store/product.actions';
 
 @Component({
-  selector: "app-product-add",
-  templateUrl: "./product-add.component.html",
-  styleUrls: ["./product-add.component.scss"]
+  selector: 'app-product-add',
+  templateUrl: './product-add.component.html',
+  styleUrls: ['./product-add.component.scss']
 })
 export class ProductAddComponent implements OnInit {
-  constructor(private productService: ProductService, private router: Router) {}
+  constructor(
+    private productService: ProductService,
+    private store: Store<ProductState>
+  ) {}
 
   ngOnInit() {}
 
   onSubmit(f: NgForm) {
-    const productObserver = {
-      next: product => (
-        this.router.navigate(["/product/list"]), console.log("success")
-      ),
-      error: err => console.error(err)
-    };
-
-    this.productService.createProduct(f.value).subscribe(productObserver);
+    this.store.dispatch(addProduct({ product: f.value }));
   }
 }
